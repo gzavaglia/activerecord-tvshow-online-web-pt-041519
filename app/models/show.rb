fee485625ab17a2254e08051f6e2b43f90c9bed2
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-06-30 23:46:01 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -10,4 +10,8 @@ class Show < ActiveRecord::Base
   def Show::lowest_rating
     Show.minimum(:rating)
   end
+  
+  def Show::least_popular_show
+    Show.find_by(rating: lowest_rating)
+  end
 end
